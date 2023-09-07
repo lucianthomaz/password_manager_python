bc269a5aa3866df485e0357ecc6c605f7d934b0e
Thomaz, Lucian (ESI) <lucian.thomaz@adp.com>
2023-09-07 11:34:06 -0300

    Adding the logo

---
+++ main.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+
+window = Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20)
+
+canvas = Canvas(width=200, height=200)
+logo_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo_img)
+canvas.grid(column=0, row=0)
+
+
+window.mainloop()
